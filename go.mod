37de44b1cbbdc0dc38a8955840725cee2f75a9e1
Tim Wang <weihewang2012@gmail.com>
2019-09-30 12:04:37 -0700

    log put segments errors

--- go.mod
+++ go.mod
@@ -3,8 +3,10 @@ module github.com/aws/aws-xray-sdk-go
 require (
 	github.com/DATA-DOG/go-sqlmock v1.2.0
 	github.com/aws/aws-sdk-go-v2 v0.10.0
-	github.com/jteeuwen/go-bindata v3.0.7+incompatible // indirect
 	github.com/pkg/errors v0.8.1
+	github.com/sirupsen/logrus v1.4.2
 	github.com/stretchr/testify v1.2.2
 	golang.org/x/net v0.0.0-20190311183353-d8887717615a
 )
+
+go 1.13
--- xray/custom_emitter.go
+++ xray/custom_emitter.go
@@ -3,6 +3,7 @@ package xray
 import (
 	"context"
 	"github.com/aws/aws-sdk-go-v2/service/xray"
+	log "github.com/sirupsen/logrus"
 	"sync"
 )
 
@@ -10,7 +11,10 @@ import (
 func EmitSegments(client *xray.Client, ctx *context.Context, batch *[]string, group sync.WaitGroup) {
 	req := &xray.PutTraceSegmentsInput{TraceSegmentDocuments: *batch}
 	putSegReq := client.PutTraceSegmentsRequest(req)
-	_, _ = putSegReq.Send(*ctx)
+	_, err := putSegReq.Send(*ctx)
+	if err != nil {
+		log.Errorf(err.Error())
+	}
 
 	group.Done()
 }
